de7c58d96d4ebe1aa2f024160b45b0fec54b438d
Akter Hossain Sohag <akterhossain.dsi@gmail.com>
2014-09-22 18:08:44 +0600

    progressing Array.prototype

--- dfine.js
+++ dfine.js
@@ -1,37 +1,42 @@
+//TODO: implement jQuery functions as dfineUtil
 (function() {
 
-    var df9;
-    df9 = function() {};
-    df9.extend = function() {
-        var i, deep = true, target = arguments[0];
-        if(typeof target === "boolean") {
-            i = 2;
-            deep = arguments[0];
-            target = arguments[1];
+    var _array = Array.prototype;
+    _array.each = function(callback) {
+        var _this = this;
+        for(var i = 0; i < _this.length; i++) {
+            callback.call(_this, _this[i], i)
         }
-        if(deep) {
-            for(; i < arguments.length; i++) {
-                for(var key in arguments[i]) {
-                    if(arguments[i].hasOwnProperty(key)) {
-                        target[key] = arguments[i][key];
-                    }
-                }
-            }
-        } else {
-            for(var key in b) {
-                if(b.hasOwnProperty(key)) {
-                    a[key] = b[key];
-                }
-            }
-        }
-
-        return target;
     };
-    df9.clazz = function(props) {
-        var Class = function(){};
-        Object.extend(props)
+    _array.map = function(callback) {
+        var _this = this;
+        _this.each(function(v, i) {
+            _this[i] = callback.call(_this, v, i);
+        })
     };
 
-    window.dFine = df9;
+    _array.trim = function() {
+        var _this = this;
+        _this.each(function(v, i) {
+            if(typeof v == 'string') {
+                _this[i] = v.trim();
+            }
+        });
+    };
 
+    var dFine;
+    dFine = function() {};
+    dFine.types = [];
+    dFine.clazz = function(origin, prototype) {
+        var heir = origin.split(' extends ')[0],
+            parents = origin.split(' extends ')[1];
+        if(parents) {
+            parents = parents.split(',').trim()
+        }
+        var Type = function(params) {
+            //this
+        };
+        $.extend(Clazz.prototype, prototype);
+        return Clazz;
+    };
 })();
